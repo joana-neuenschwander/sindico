3429cbf1f7ca81a7d06346883ee7e1b1c5630f22
Joana Neuenschwander <jo.neush@gmail.com>
2024-06-10 17:19:48 -0300

    Create Appointments Model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   # Colocar as validações (número de caracteres máximo, etc, quais atributos, se a data final é depois da inicial)
+  has_many :appointments
   validates :name, :description, presence: true
 end
